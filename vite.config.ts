04080db2060558939439f6e5c8fcc7b46e457aa5
Rupesh <avgsmutenjoyer@gmail.com>
2026-02-10 21:56:39 +0530

    configure new stuff

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/Sawariya-Diagnostic/",
+  base: "/sawariyadiagnostic/",
   server: {
     host: "::",
     port: 8080,
